3ae2e727d44b642446509229a3cc1c09957d2e77
Pankaj Gupta <pankaj392pg@gmail.com>
2025-08-05 19:35:02 +0530

    fix: navbar

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,61 +1,3 @@
-// import { useContext} from "react"
-// import { NavLink, useNavigate } from "react-router-dom"
-// import MovieProvider from "../../context/MovieProvider"
-// import './Nav.css'
-
-// const Nav = () => {
-
-//     const {getSearchMovies, searchValue, setSearchValue, setPage} = useContext(MovieProvider)
-
-    
-
-//     const navigate = useNavigate()
-
-
-//     function searchHandler(){
-
-//         if(searchValue.trim()){
-//             setPage(1)
-//             getSearchMovies(searchValue)
-//             navigate('/search')
-//         }
-
-           
-//     }
-
-    
-    
-//     return(
-
-    
-//     <nav className="navbar">
-//   <NavLink className="navbar-logo" to="/">MovieDB</NavLink>
-
-//   <ul className="navbar-links">
-//     <li><NavLink to="/" className="nav-link">Popular</NavLink></li>
-//     <li><NavLink to="/topRated" className="nav-link">Top Rated</NavLink></li>
-//     <li><NavLink to="/upcoming" className="nav-link">Upcoming</NavLink></li>
-//     <li>
-//       <div className="search-container">
-//         <input
-//           type="text"
-//           className="search-input"
-//           placeholder="Movie Name"
-//           value={searchValue}
-//           onChange={(e) => setSearchValue(e.target.value)}
-//         />
-//         <button onClick={() => searchHandler()} className="search-button">Search</button>
-//       </div>
-//     </li>
-//   </ul>
-// </nav>
-
-//     )
-// }
-
-// export default Nav
-
-
 
 import { useContext, useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
@@ -87,19 +29,19 @@ const Nav = () => {
 
   return (
     <nav className="navbar">
-      {/* Logo */}
+    
       <NavLink className="navbar-logo" to="/">
         MovieDB
       </NavLink>
 
-      {/* Hamburger icon for mobile */}
+      
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
 
-      {/* Navigation links */}
+     
       <ul className={`navbar-links ${menuOpen ? "show" : ""}`}>
         <li>
           <NavLink
